1e33efe69082b50409db48ea2596cfcc2b1cd646
Stephane <son.trinh.dev@gmail.com>
2017-12-28 09:03:50 +0000

    automatically backed up by learn

--- variables.js
+++ variables.js
@@ -3,3 +3,13 @@ function speaker() {
 }
 speaker();
 console.log(sentence);
+
+var cuteAnimal = 'quokka';
+
+function makeVariable() {
+  var cuteAnimal = 'sugar glider';
+  return cuteAnimal;
+}
+
+console.log(`Variable from function cuteAnimal = ` + makeVariable())
+console.log(`Variable from varaiable cuteAnimal = ${cuteAnimal}`)
